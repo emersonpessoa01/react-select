542f4a2fe6c997aca34e53ff7b195edb24565a56
emersonpessoa01 <eps011108@gmail.com>
2020-11-28 19:58:39 -0300

    try adjust again

--- src/App.js
+++ src/App.js
@@ -3,27 +3,48 @@ import M from "materialize-css";
 import PERIODS from "./helpers/periods";
 
 export default function App() {
+  const [currentPeriod, setCurrentPeriod] = React.useState(PERIODS[0]);
+  const [transactions, setTransactions] = React.useState([]);
 
- 
+  React.useEffect(() => {
+    const fetchUrl = async ()=>{
+      const url ="https://api-transaction-chanceller.herokuapp.com/transaction"
+      const periodUrl = `${url}?period=${currentPeriod}`
+
+      const resource = await fetch(periodUrl)
+      const json = await resource.json();
+      console.log(json)
+
+      setTransactions(json)
+    }
+    
+    fetchUrl()
+  },[currentPeriod])
+
   React.useEffect(() => {
     M.AutoInit();
   }, []);
 
-  const handleSelectChange = (evt) => {
-    const newPeriod = evt.target.value;
+  const handleSelectChange=(event)=>{
+    const newPeriod = event.target.value;
     setCurrentPeriod(newPeriod);
-  };
-
+  }
+  
   return (
     <div className="container">
       <h1 className="center">React Select</h1>
-
-      <select value={currentPeriod} onChange={handleSelectChange}>
+      <select vaue={currentPeriod} onChange={handleSelectChange}>
         {PERIODS.map((period) => {
-          return <option key={period}>{period}</option>;
+          return (
+            <option key={period} value={period}>
+              {period}
+            </option>
+          );
         })}
       </select>
-
+      <div>{transactions.map((id,description) =>{
+        return (<p key={id}>{description}</p>)
+      })}</div>
     </div>
   );
 }
